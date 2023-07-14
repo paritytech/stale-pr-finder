8cb2bfa7eacf2d69f43a1e619f64368ef8a8d2ea
Javier Bullrich <javier.bullrich@parity.io>
2023-07-14 14:18:50 +0200

    Implemented linting (#10)
    Implemented
    [`@eng-automation/js-style`](https://github.com/paritytech/opstooling-js-style)
    with the linting method.
    
    Created CI step for it to be tested.
    
    Closes #4

--- src/filters.ts
+++ src/filters.ts
@@ -1,14 +1,12 @@
 import moment from "moment";
+
 import { PullRequest } from "./types";
 
-export const olderThanDays = (pr: PullRequest, daysStale: number): boolean => {
-    return moment().diff(moment(pr.updated_at), "days") > daysStale;
-}
+export const olderThanDays = (pr: PullRequest, daysStale: number): boolean =>
+  moment().diff(moment(pr.updated_at), "days") > daysStale;
 
-export const byNoReviews = (pr: PullRequest): boolean => {
-    return !pr.reviews || pr.reviews.length === 0;
-}
+export const byNoReviews = (pr: PullRequest): boolean => !pr.reviews || pr.reviews.length === 0;
 
-export const byLabels = (pr: PullRequest, labels: string[]): boolean => {
-    return pr.labels && pr.labels.map(l => l.name.toLowerCase()).some(l => labels.map(l => l.toLowerCase()).includes(l));
-}
+export const byLabels = (pr: PullRequest, labels: string[]): boolean =>
+  pr.labels &&
+  pr.labels.map((l) => l.name.toLowerCase()).some((l) => labels.map((label) => label.toLowerCase()).includes(l));
